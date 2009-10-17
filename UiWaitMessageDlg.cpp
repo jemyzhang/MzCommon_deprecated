8d004db9616fed4c54d93833970b27529c98a9fa
jemyzhang <jemyzhang@53aff7d1-dc1e-0346-9cbd-63131b65f07d>
2009-10-17 11:40:14 +0000

    git-svn-id: https://172.16.185.65/svn/MzCommon/trunk@8 53aff7d1-dc1e-0346-9cbd-63131b65f07d

---
+++ UiWaitMessageDlg.cpp
@@ -0,0 +1,64 @@
+#include "UiWaitMessageDlg.h"
+#include "MzCommon.h"
+
+using namespace MzCommon;
+
+MZ_IMPLEMENT_DYNAMIC(Ui_WaitMessageDlgWnd)
+
+Ui_WaitMessageDlgWnd::Ui_WaitMessageDlgWnd(const TCHAR* content){
+    m_messageText = NULL;
+    if(content){
+        C::newstrcpy(&m_messageText,content);
+    }else{
+        C::newstrcpy(&m_messageText,L"«Î…‘µ»...");
+    }
+    nRet = false;
+}
+
+Ui_WaitMessageDlgWnd::~Ui_WaitMessageDlgWnd(){
+    if(m_messageText) delete m_messageText;
+}
+
+BOOL Ui_WaitMessageDlgWnd::OnInitDialog(){
+    // Must all the Init of parent class first!
+    if (!CMzWndEx::OnInitDialog()) {
+        return FALSE;
+    }
+    m_Bg.SetPos(0,0,GetWidth(),GetHeight());
+    AddUiWin(&m_Bg);
+
+    m_Message.SetPos(10,0,GetWidth() - 20,GetHeight());
+    m_Message.SetTextSize(30);
+    m_Message.SetText(m_messageText);
+    m_Message.SetTextColor(RGB(255,255,255));
+    m_Message.SetDrawTextFormat(DT_VCENTER|DT_CENTER);
+    AddUiWin(&m_Message);
+
+    return TRUE;
+}
+
+int Ui_WaitMessageDlgWnd::DoModal(){
+    SetTimer(m_hWnd,0x1001,100,NULL);
+    return CMzWndEx::DoModal();
+}
+
+void Ui_WaitMessageDlgWnd::OnTimer(UINT nIDEvent){
+    switch(nIDEvent){
+        case 0x1001:
+            KillTimer(m_hWnd,0x1001);
+            SetTimer(m_hWnd,0x1002,100,NULL);
+            nRet = CallBackProcess();
+            break;
+        case 0x1002:
+            EndModal(nRet ? ID_OK : ID_CANCEL);
+            break;
+    }
+}
+
+void Ui_WaitMessageDlgWnd::setMessage(LPTSTR msg){
+    if(msg){
+        C::newstrcpy(&m_messageText,msg);
+    }else{
+        C::newstrcpy(&m_messageText,L"«Î…‘µ»...");
+    }
+}
---
+++ UiWaitMessageDlg.h
@@ -0,0 +1,41 @@
+#pragma once
+/*
+ * @filename UiSingleOption.h
+ * @note 等待对话框
+ * @author JEMYZHANG
+ * @date 2009.10.17
+ * @ver. 1.0
+ * @changelog
+ *  ver.1.0 初始化程序
+*/
+// include the MZFC library header file
+#include <mzfc_inc.h>
+
+class UiBackground :
+	public UiWin
+{
+public:
+	virtual void PaintWin(HDC hdcDst, RECT* prcWin, RECT* prcUpdate){
+		UiWin::PaintWin(hdcDst, prcWin, prcUpdate);
+		MzDrawGridDlgBG(hdcDst,prcWin);
+	}
+};
+
+class Ui_WaitMessageDlgWnd : public CMzWndEx{
+    MZ_DECLARE_DYNAMIC(Ui_WaitMessageDlgWnd);
+public:
+    Ui_WaitMessageDlgWnd(const TCHAR* content = NULL);
+    ~Ui_WaitMessageDlgWnd();
+public:
+    UiBackground m_Bg;
+    UiStatic m_Message;
+public:
+    BOOL OnInitDialog();
+    virtual bool CallBackProcess() = 0;
+    int DoModal();
+    void OnTimer(UINT nIDEvent);
+    void setMessage(LPTSTR msg);
+private:
+    bool nRet;
+    LPTSTR m_messageText;
+};
